e83384275db1a7da1218f337c4c3ba6cac9150bd
Paul Solovey <p.v.solovey@ya.ru>
2024-01-13 11:31:16 +0300

    process.env.PORT added

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ router.delete('/', (ctx) => {
   ctx.body = ticketList.getAllTickets();
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
